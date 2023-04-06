66165601fb4ab2e0604927f8fbbdefafc711bf9d
박정욱 <jupark0369@gmail.com>
2023-04-06 16:21:04 +0900

    bitwiseOperator added

---
+++ bitwiseOperator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int A = 3;
+  printf("%d\n", A << 2);
+  printf("%d\n", A << 2);
+  printf("%d\n", A << 3);
+  printf("%d\n", A << 1);
+  printf("%d\n", A << 0);
+  printf("%d\n", A >> 1);
+  printf("%d\n", A >> 2);
+  printf("%d\n", A >> 3);
+}
