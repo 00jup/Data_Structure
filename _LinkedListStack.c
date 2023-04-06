4982fd86c5418025a060d47f9312c8a187ffcd30
박정욱 <jupark0369@gmail.com>
2023-04-06 19:46:31 +0900

    before modified

--- _LinkedListStack.c
+++ _LinkedListStack.c
@@ -71,7 +71,7 @@ int size(Stack *s)
 {
 
   int size = 0;
-  for (Node *p = s->top; p != NULL; p = p->link) // 여기서 p->link랑 p != NULL 차이가 뭐지.
+  for (Node *p = s->top; p != NULL; p = p->link) // 여기서 p->link랑 p != NULL
   {
     size++;
   }
@@ -83,7 +83,6 @@ int main()
   scanf("%d", &N);
   Stack *s = (Stack *)malloc(sizeof(Stack));
   char *cmd = (char *)malloc(sizeof(char) * N);
-  char *result = (char *)malloc(sizeof(char) * N);
   for (int i = 0; i < N; i++)
   {
     scanf("%s", (cmd + i));
@@ -91,36 +90,32 @@ int main()
     {
       scanf("%d", &num);
       push(s, num);
-      *(result + i) = s->top->item;
+      printf("%d\n", s->top->item);
     }
     else if (strcmp((cmd + i), "top") == 0)
     {
       if (is_empty(s))
-        *(result + i) = -1;
+        printf("-1\n");
       else
-        *(result + i) = s->top->item;
+        printf("%d\n", s->top->item);
     }
     else if (strcmp((cmd + i), "size") == 0)
     {
-      *(result + i) = size(s);
+      printf("%d\n", size(s));
     }
     else if (strcmp((cmd + i), "emtpy") == 0)
     {
-      if (is_empty(s))
-        *(result + i) = 1;
+      if (is_empty(s) == -1)
+        printf("1\n");
       else
-        *(result + i) = 0;
+        printf("0\n");
     }
     else if (strcmp((cmd + i), "pop") == 0)
     {
-      *(result + i) = pop(s);
+      printf("%d\n", pop(s));
     }
   }
-  for (int j = 0; j < N; j++)
-  {
-    printf("%d\n", *(result + j));
-  }
+
   free(s);
-  free(result);
   return 0;
 }
