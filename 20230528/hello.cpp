4d9e4cb6748fec2c825b0a501c4a740545579043
00jup <jupark0369@gmail.com>
2023-05-30 16:32:37 +0900

    dfs and unistd

--- 20230528/hello.cpp
+++ 20230528/hello.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include "BigInt.hpp"
 
-using namespace std;
 
 int main()
 {
-  cout << "hello world!" << endl;
+  std::cout << "hello world!" << std::endl;
 }
