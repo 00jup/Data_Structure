287f30eab3af5029566767714efd2823daf8cb1d
00jup <jupark0369@gmail.com>
2023-06-02 23:16:08 +0900

    very basic terminal command

---
+++ 20230602/t_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void ft_ft(int *nbr);
+
+int main(void)
+{
+    int *p_nbr;
+    int nbr;
+
+    nbr = 21;
+    p_nbr = &nbr;
+
+    ft_ft(p_nbr);
+    printf("%d", nbr);
+
+    return 0;
+}
